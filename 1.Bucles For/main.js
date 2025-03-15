592010a2812fd253801b14f06007a1d574f2bd48
GLJROB3 <cshtd.computacion@gmail.com>
2025-03-15 05:56:48 -0600

    Add bucles for

---
+++ 1.Bucles For/main.js
@@ -0,0 +1,25 @@
+//recorrer los números del 1 al 10 usando For
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+//Imprime los números pares entre 0 y 20 usando for y una condición if.
+
+for (let i = 0; i <= 20; i++) {
+    if ( i % 2 == 0)
+        console.log(i);
+};
+
+//Crea un triángulo con asteriscos usando bucles anidados
+
+let filas = 5;
+
+for(let i = 1; i <= filas; i++){
+    let espacio = "";
+
+for (let h = 1; h <= i; h++) {
+    espacio += "*";
+}
+    console.log(espacio);
+}
