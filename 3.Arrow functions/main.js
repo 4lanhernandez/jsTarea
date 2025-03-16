72718fa98aed05a455e30d2691373382bc383067
GLJROB3 <cshtd.computacion@gmail.com>
2025-03-16 01:29:22 -0600

    add arrow functions:truck:

---
+++ 3.Arrow functions/main.js
@@ -0,0 +1,21 @@
+//Ejercicio 3.1: Convierte esta función a arrow function.
+
+function sumar(a, b) {
+    return a + b;
+  }
+  console.log(sumar(4,5));
+
+  //arrow function
+
+const sumar = (a, b) => a + b;
+    console.log(sumar(4,5));
+
+
+
+//Ejercicio 3.2: Crea una arrow function que reciba un número y devuelva su cuadrado.
+
+const cuadrado = (a) => a * a;
+console.log(cuadrado(4));
+
+
+//Ejercicio 3.3 (Desafío): Usa una arrow function con map() para duplicar los valores del array [2, 4, 6]
