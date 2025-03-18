a0bf72f10c4e3376c6f0a7be78e2883f8c5b20b1
GLJROB3 <cshtd.computacion@gmail.com>
2025-03-18 07:45:55 -0600

    add Async await :safety_vest:

---
+++ 6.Async await/main.js
@@ -0,0 +1,23 @@
+// Ejercicio 6.1: Crea una función esperarSegundos(segundos) que use setTimeout con async/await
+
+function esperarSegundos(segundos) {
+    return new Promise(esperar => setTimeout(esperar, segundos * 1000));
+}
+
+async function mostrarMensajes() {
+    console.log("Inicio de la mensajería");
+
+    await esperarSegundos(3);
+    console.log("Primer mensaje después de 3 segundos")
+
+    await esperarSegundos(6);
+    console.log("Segundo mensaje después de 6 segundos")
+
+    await esperarSegundos(2);
+    console.log("Tercer mensaje después de 2 segundos mas")
+
+    console.log("Fin de la mensajería")
+}
+    console.log("Mensaje antes del INICIO")
+    mostrarMensajes();
+    console.log("Mensaje después de llamar la función")
